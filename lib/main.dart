5aea27e64cd4d934a6c2322bb1a04f1cccb4393b
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2025-06-04 15:00:04 +0300

    Changed background color in appBar.

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.orange,
+          backgroundColor: Colors.pink,
           title: Text('Get Test'),
           centerTitle: true,
         ),
